06acc84630d401f9db579fd6a634928dda899134
vaaghu <vaaghu0@gmail.com>
2024-04-03 16:50:08 +0530

    add paytm

---
+++ CAS/utils/paytm.py
@@ -0,0 +1,92 @@
+import pandas as pd
+import re
+import tabula
+
+
+def currencyToFloat(text):
+    text = re.sub(r"[(].*[)]", "", text)
+    text = re.sub(r"[\s,₹]", "", text)
+    return float(text)
+
+
+def paytm(fileURL, PASSWORD):
+
+    pdf = tabula.read_pdf(fileURL, password=PASSWORD, pages="all")
+    part_one = None
+    part_two = None
+    for tabel in pdf:
+        try:
+            if tabel.shape[1] < 5:
+                continue
+            # print(tabel.shape)
+            tabel = pd.DataFrame(tabel)
+
+            portfolio_index = (
+                tabel["Unnamed: 0"]
+                .str.contains("^Portfolio Details")
+                .apply(lambda x: True if x == True else False)
+                .idxmax()
+            )
+            if portfolio_index > 0:
+                part_one = tabel[portfolio_index + 2 : -1]
+
+            total_index = (
+                tabel["Unnamed: 0"]
+                .str.contains("^Total")
+                .apply(lambda x: True if x == True else False)
+                .idxmax()
+            )
+            if total_index > 0:
+                part_two = tabel[1:total_index]
+                break
+
+        except:
+            pass
+
+    result = (
+        part_one[
+            [
+                "Unnamed: 0",
+                "Unnamed: 1",
+                "Unnamed: 2",
+                "Unnamed: 3",
+                "Unnamed: 4",
+                "Unnamed: 5",
+            ]
+        ]
+        .append(
+            part_two[
+                [
+                    "Unnamed: 0",
+                    "Unnamed: 1",
+                    "Unnamed: 2",
+                    "Unnamed: 3",
+                    "Unnamed: 4",
+                    "Unnamed: 5",
+                ]
+            ],
+            ignore_index=True,
+        )
+        .rename(
+            columns={
+                "Unnamed: 0": "fund",
+                "Unnamed: 1": "units",
+                "Unnamed: 2": "NAV",
+                "Unnamed: 3": "invested",
+                "Unnamed: 4": "current value",
+                "Unnamed: 5": "absolute",
+            }
+        )
+    )
+
+    result["invested"] = result["invested"].apply(currencyToFloat)
+    result["NAV"] = result["NAV"].apply(currencyToFloat)
+    result["current value"] = result["current value"].apply(currencyToFloat)
+
+    result["absolute"] = result["absolute"].apply(currencyToFloat)
+    result["absolute percent"] = round(
+        (result["absolute"] / result["invested"]) * 100, 2
+    )
+
+    result = result.astype({"fund": "string", "units": "float"})
+    return result
--- webscrap/settings/__init__.py
+++ webscrap/settings/__init__.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e+c6u&c8pdi$yff18mk58%74i*@9xo8@u+&ahr*5z!znz+089!'
+SECRET_KEY = "django-insecure-e+c6u&c8pdi$yff18mk58%74i*@9xo8@u+&ahr*5z!znz+089!"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,61 +31,60 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'django_extensions',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "django_extensions",
     "rest_framework",
-
     "cas",
-    "news_extraction"
+    "news_extraction",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'webscrap.urls'
+ROOT_URLCONF = "webscrap.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'webscrap.wsgi.application'
+WSGI_APPLICATION = "webscrap.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-      'ENGINE': 'django.db.backends.postgresql',
-      'NAME': 'news_extractions',
-      'PORT': '5432',
-      'HOST': 'localhost',
-      'PASSWORD': '123456',
-      'USER': 'postgres'
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "news_extractions",
+        "PORT": "5432",
+        "HOST": "localhost",
+        "PASSWORD": "123456",
+        "USER": "postgres",
     }
 }
 
@@ -99,16 +98,16 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
@@ -116,9 +115,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'Asia/Kolkata'
+TIME_ZONE = "Asia/Kolkata"
 
 USE_I18N = True
 
@@ -128,9 +127,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
